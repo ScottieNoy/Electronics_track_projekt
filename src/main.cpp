df2c92b57dae2608301fa446c87a5f347edb9159
ScottieNoy <91637465+ScottieNoy@users.noreply.github.com>
2023-03-09 15:59:28 +0100

    with serial print according to data management

--- src/main.cpp
+++ src/main.cpp
@@ -31,75 +31,24 @@ void setup(void) {
   while (!Serial)
     delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit MPU6050 test!");
+  // Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
+    // Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
     }
   }
-  Serial.println("MPU6050 Found!");
+  // Serial.println("MPU6050 Found!");
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+  
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
+  
 
   mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+  
 
   Serial.println("");
   delay(100);
@@ -111,27 +60,37 @@ void loop() {
   mpu.getEvent(&a, &g, &temp);
 
   /* Print out the values */
-  Serial.print("Acceleration X: ");
+  // Serial.print("Acceleration X: ");
   Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
+  Serial.print(";");
+  // Serial.print(", Y: ");
   Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
+  Serial.print(";");
+
+  // Serial.print(", Z: ");
   Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+  Serial.print(";");
 
-  Serial.print("Rotation X: ");
+  // Serial.println(" m/s^2");
+
+  // Serial.print("Rotation X: ");
   Serial.print(g.orientation.roll);
-  Serial.print(", Y: ");
+  Serial.print(";");
+
+  // Serial.print(", Y: ");
   Serial.print(g.orientation.pitch);
-  Serial.print(", Z: ");
+  Serial.print(";");
+  // Serial.print(", Z: ");
   Serial.print(g.orientation.heading);
-  Serial.println(" degrees");
+  Serial.print(";");
+  // Serial.println(" degrees");
 
-  Serial.print("Temperature: ");
+  // Serial.print("Temperature: ");
   Serial.print(temp.temperature);
-  Serial.println(" degC");
+  Serial.print(";");
+  // Serial.println(" degC");
 
-  Serial.println("");
+  // Serial.println("");
 
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -146,9 +105,9 @@ void loop() {
   distance = duration * 0.034 / 2;
 
   // print the distance to the serial monitor
-  Serial.print("Distance: ");
+  // Serial.print("Distance: ");
   Serial.print(distance);
-  Serial.println(" cm");
+  // Serial.println(" cm");
   printToDisplay(g,distance);
   delay(500);
 }
